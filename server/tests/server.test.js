9490b19e84838bbe8b0ea5e85d3e45c07edac242
jorgitoml <jorgito_ml@hotmail.com>
2018-11-16 19:13:35 +0100

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -229,7 +229,7 @@ describe('POST /users',()=>{
                     expect(user).toBeTruthy();
                     expect(user.password).not.toBe(password);
                     done();
-                });
+                }).catch((e)=>done(e));
             });
 
     });
@@ -249,9 +249,59 @@ describe('POST /users',()=>{
         const email = test_users[0].email;
         const password = test_users[0].password;
         request(app)
-        .post('/users')
-        .send({email, password})
-        .expect(400)
-        .end(done);
+            .post('/users')
+            .send({email, password})
+            .expect(400)
+            .end(done);
+    });
+   
+});
+
+describe('POST /users/login',()=>{
+    it('should login user and return auth token',(done)=>{
+        const email = test_users[1].email;
+        const password = test_users[1].password;
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.headers['x-auth']).toBeTruthy();
+            })
+            .end((err,res)=>{
+                if(err) return done(err);
+
+                User.findById(test_users[1]._id).then((user)=>{
+                    expect(user.tokens[0]).toMatchObject(
+                        {
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        }
+                    );
+                    done();
+                }).catch((e)=>done(e));
+            });
+    });
+
+    it('should reject invalid login',(done)=>{
+        const email = test_users[1].email;
+        const password = 'invalid_pass';
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(400)
+            .expect((res)=>{
+                expect(res.headers['x-auth']).toBeFalsy();
+            })
+            .end((err,res)=>{
+                if(err) return done(err);
+
+                User.findById(test_users[1]._id).then((user)=>{
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e)=>done(e));
+            });
     });
 });
